aa82f911a2bb54db4c586d344330e853ebcedadf
Carlos Valencia <valenciacarlos278@gmail.com>
2023-08-28 20:26:24 -0500

    Creacion y listado de tareas

--- main.py
+++ main.py
@@ -31,12 +31,62 @@ def login():
     password = model["password"]
     connection = sqlite3.connect("maindb.db",check_same_thread=False)
     cursor = connection.cursor()
-    rows = cursor.execute("SELECT * FROM USERS WHERE email='{}' AND password='{}'".format(email,password))
-    if(len(list(rows)) != 1):
-        return "{'error':'Usuario no encontrado'}",404
+    rows = cursor.execute("SELECT * FROM USERS WHERE email='{}' AND password='{}' LIMIT 1".format(email,password))
+    user_response = {
+        'username': '',
+        'email': '',
+        'id': 0
+    }
+    for user in rows:
+        user_response["username"] = user[0]
+        user_response["email"] = user[1]
+        user_response["id"] = user[3]
     
-    return model,200
+    return user_response,200
 
+
+@app.route('/createtask',methods=['POST'])
+def create_task():
+    if request.method == 'POST':
+        model = request.get_json()
+        user_id = ""
+        name = ""
+        content = ""
+        try:
+            user_id = model["id_user"]
+            name = model["name"]
+            content = model["content"]
+            
+            
+        except:
+            return Response("{'error':'Tarea no registrada'}",status=400,mimetype='application/json')
+        connection = sqlite3.connect("maindb.db",check_same_thread=False)
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO TASKS (user_id,NAME,CONTENT) VALUES ({},'{}','{}')".format(user_id,name,content))
+        connection.commit()
+        cursor.close()
+        connection.close()
+    
+        return Response("{'status':'Tarea fue registrada'}",status=201, mimetype='application/json')
+    
+
+@app.route('/tasklist/<userid>')
+def tasklist(userid=0):
+    tasks = []
+    connection = sqlite3.connect("maindb.db",check_same_thread=False)
+    cursor = connection.cursor()
+    rows = cursor.execute("SELECT * FROM TASKS WHERE user_id={}".format(userid))
+    for task in rows:
+        task = {
+            'task_id': task[0],
+            'name': task[2],
+            'content': task[3]
+        }
+        tasks.append(task)
+    connection.commit()
+    cursor.close()
+    connection.close()
+    return tasks,200
 
 
 
@@ -53,7 +103,7 @@ def register():
             username = model["username"]
             connection = sqlite3.connect("maindb.db",check_same_thread=False)
             cursor = connection.cursor()
-            cursor.execute("INSERT INTO USERS VALUES('{}','{}','{}')".format(username,email,password))
+            cursor.execute("INSERT INTO USERS (username,email,password) VALUES('{}','{}','{}')".format(username,email,password))
             connection.commit()
             cursor.close()
             connection.close()
